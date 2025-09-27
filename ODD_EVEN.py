d9aa5536e01228341b4d75aa5d3901ad589ab4b8
SIDDHESH UGALE <151725299+SiddheshUgale73@users.noreply.github.com>
2025-09-27 23:34:07 +0530

    Add files via upload

---
+++ ODD_EVEN.py
@@ -0,0 +1,7 @@
+# Write a program to check whether the given number is odd or even
+
+num= int(input ('enter a number: '))
+if num%2==0:
+    print(f'{num} is even')
+else:
+    print(f'{num} is odd')
---
+++ simple_nested_if.py
@@ -0,0 +1,7 @@
+age = 18
+country = 'india'
+if age >= 18:
+    if country == 'india':
+        print("You are eligible to vote in India.")
+    else:
+        print("You are eligible to vote, but not in India.")
